e5aa64fabb660044350232e47f2ebbde4d72a512
Yash Patankar <yashpatankar483@gmail.com>
2024-12-19 23:59:24 +0530

    included the modulo and print statements

--- pythonProject/calci.py
+++ pythonProject/calci.py
@@ -10,3 +10,11 @@ def multiply(num1, num2):
 
 def div(num1, num2):
     return num1 / num2
+
+def modulo(num1, num2):
+    return num1 % num2
+
+print(add(num1,num2))
+print(multiply(num1,num2))
+print(div(num1,num2))
+print(modulo(num1,num2))
