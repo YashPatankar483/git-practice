518a4905f5e9d5883781f3596fbfeb129231552c
Yash Patankar <yashpatankar483@gmail.com>
2024-12-19 23:10:48 +0530

    need only add iqqs reverts commit ae334818e423d6567b7891110d95ab359bba2154.

--- pythonProject/calci.py
+++ pythonProject/calci.py
@@ -5,11 +5,4 @@ num2 = int(input("Enter any number: "))
 def add(num1, num2):
     return num1 + num2
 
-def sub(num1, num2):
-    return num1 - num2
 
-def multiply(num1, num2):
-    return num1 * num2
-
-def dev(num1, num2):
-    return num1 * num2
