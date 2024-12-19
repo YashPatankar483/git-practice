06d2694d2dbae80f9fc4865912c772502effac19
Yash Patankar <yashpatankar483@gmail.com>
2024-12-19 23:32:51 +0530

    removed sub

--- pythonProject/calci.py
+++ pythonProject/calci.py
@@ -5,10 +5,6 @@ num2 = int(input("Enter any number: "))
 def add(num1, num2):
     return num1 + num2
 
-
-def substraction(num1, num2):
-    return num1 - num2
-
 def multiply(num1, num2):
     return num1 * num2
 
